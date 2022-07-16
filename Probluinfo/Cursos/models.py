60ec34b92bf4d0e55edbc798a5d11dd125478557
Edson-Padilha <edsonp.bsi@gmail.com>
2022-07-16 17:33:00 -0300

    Subindo tabela cursos

--- Probluinfo/Cursos/models.py
+++ Probluinfo/Cursos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Cursos(models.Model):
+    nm_curso = models.CharField(max_length=50, blank=False, unique=True, primary_key=True)
+    carga_horaria = models.IntegerField(max_length=3 ,blank=False)
+    vl_curso = models.FloatField(max_length=8, blank=False)
+
+    class Meta:
+        db_table = 'Cursos'
+
+    def __str__(self):
+        return self.nm_curso
+            
