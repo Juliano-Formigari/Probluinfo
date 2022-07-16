7577981dd84dd7e33db33e4b2cdafabc4fefb63c
Edson-Padilha <edsonp.bsi@gmail.com>
2022-07-16 18:00:50 -0300

    Subindo tabela Caixa.

--- Probluinfo/Financeiro/models.py
+++ Probluinfo/Financeiro/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Caixa(models.Model):
+    class Tipos(models.IntegerChoices):
+        Entrada = '1'
+        Saída = '2'
+    tipo = models.IntegerField(choices=Tipos.choices)
+    descricao = models.CharField(max_length=50, blank=False)
+    valor = models.FloatField(max_length=10, blank=True)
+
+    class Meta:
+        db_table = 'Caixa'
+    
+    def __str__(self):
+        return self.tipo
