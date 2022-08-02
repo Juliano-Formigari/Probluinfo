4085b4ed6f38f0b974da4442e4bd78c5c0e1dbcb
Edson-Padilha <edsonp.bsi@gmail.com>
2022-08-01 22:17:31 -0300

    Subindo arquivo cadastra cursos

--- Probluinfo/Cursos/views.py
+++ Probluinfo/Cursos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from Cursos.forms import FormCursos
 
 # Create your views here.
 def cadastra_salas(request):
     return render(request,'cadastra_salas.html')
 
 def cadastra_cursos(request):
+    if request.method == 'POST':
+        form = FormCursos(request.POST or None)
+        if form.is_valid():
+            form.save()
     return render(request,'cadastra_cursos.html')
 
 def cadastra_matriculas(request):
