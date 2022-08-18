54740ec95beef3de63b451c6053a96f1244f13f9
Juliano Lemes Formigari <60493741+Juliano-Formigari@users.noreply.github.com>
2022-08-18 12:03:45 -0300

    Ajustando regra de cadastro pessoas (#11)

--- Probluinfo/Pessoas/models.py
+++ Probluinfo/Pessoas/models.py
@@ -1,34 +1,30 @@
 from django.db import models
+# from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class Pessoas(models.Model):
-    Aluno = 1
-    Instrutor = 2
-    Vendedor = 3
-    Administrador = 4
+class Perfis(models.Model):
+    descricao = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=True)
 
-    PERFIS_CHOICES = (
-        (Aluno,'Aluno'),
-        (Instrutor,'Instrutor'),
-        (Vendedor,'Vendedor'),
-        (Administrador,'Administrador')
-    )
+    class Meta:
+        db_table = 'Perfis'
 
-    nm_completo = models.CharField(max_length=100, blank=False) # Nome completo
-    cpf = models.CharField(max_length=14, blank=False, unique=True) # CPF somente numeros
-    email = models.EmailField(max_length=50, blank=True, unique=True) # Email com validação
-    dt_nascimento = models.DateField(max_length=10, blank=False) # Somente numeros, pois no banco salva com '/'
-    celular = models.CharField(max_length=15, blank=True, unique=True) # Somente numeros
-    fone_res = models.CharField(max_length=15, blank=True) # Telefone resindencial, somente numeros
-    nm_resp = models.CharField(max_length=100, blank=True) # Nome da mãe, ou responsável caso não tenha, e seja menor de idade
-    login = models.CharField(max_length=50, blank=False, unique=True)
-    senha = models.CharField(max_length=50, blank=False)
-    perfil = models.IntegerField(blank=False,choices=PERFIS_CHOICES)
-    dt_alteracao = models.DateField(auto_now=True) # Data de registro de alteração do cadastro
+    def __str__(self):
+        return self.descricao
+
+class Pessoas(AbstractUser):
+    cpf = models.CharField(max_length=14, blank=False, unique=True)
+    dt_nascimento = models.DateField(max_length=10, blank=False)
+    celular = models.CharField(max_length=15, blank=True, unique=True)
+    fone_res = models.CharField(max_length=15, blank=True)
+    nm_resp = models.CharField(max_length=100, blank=True)
+    dt_alteracao = models.DateField(auto_now=True)
+    id_perfil = models.ForeignKey(Perfis, on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'Pessoas'
 
     def __str__(self):
-        return self.nm_completo
+        return self.first_name
     
