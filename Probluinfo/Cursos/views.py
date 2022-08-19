2adecb1a183247f16f756b39fc8312f12c3b46ae
Juliano-Formigari <julianomix@gmail.com>
2022-08-19 11:18:03 -0300

    Subindo alterações

---
+++ Probluinfo/Cursos/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render,redirect
+from Cursos.forms import FormCursos,FormMatriculas,FormNotas,FormSalas
+from Cursos.models import Matriculas,Cursos,Salas,Notas
+from ViewsProject.views import efetua_paginacao
+from django.views.decorators.http import require_POST
+
+
+# Create your views here.
+
+def cadastra_cursos(request):
+    if request.method == 'POST':
+        form = FormCursos(request.POST or None)
+        if form.is_valid():
+            form.save()
+    return render(request,'cadastra_cursos.html')
+    
+def cadastra_matriculas(request):
+    periodos = Matriculas.PERIODOS_CHOICES
+    if request.method == 'POST':
+        form = FormMatriculas(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect(cadastra_matriculas)
+
+    dados = {
+                'periodos': periodos,
+            }
+    return render(request,'cadastra_matriculas.html', dados)
+
+def cadastra_salas(request):
+    if request.method == 'POST':
+        form = FormSalas(request.POST or None)
+        if form.is_valid():
+            form.save()
+    return render(request,'cadastra_salas.html')
+
+def cadastra_notas(request):
+    if request.method == 'POST':
+        form = FormNotas(request.POST or None)
+        if form.is_valid():
+            form.save()
+    return render(request,'cadastra_notas.html')
+
+
+def lista_cursos(request):
+    
+    procura= request.GET.get('procura')
+    if procura:
+        cursos = Cursos.objects.filter(cursos__icontains=procura)
+    else:
+        cursos = Cursos.objects.all()
+    
+    total = cursos.count
+
+    dados = {
+                'cursos' : cursos,
+                'total' : total, 
+                'procura' : procura,
+                'porPagina' : efetua_paginacao(request, cursos)
+            }
+
+    return render(request,'lista_cursos.html',dados)
+
+def lista_matriculas(request):
+    procura = request.GET.get('procura')
+
+    if procura:
+        matricula = Matriculas.objects.filter(nome__icontains=procura)|Matriculas.objects.filter(email__icontains=procura)
+    else:
+        matricula = Matriculas.objects.all()
+
+    total = matricula.count
+
+    dados = {
+                'matricula' : matricula, 
+                'total' : total, 
+                'procura' : procura,
+              
+            }
+
+    return render(request, 'lista_matriculas.html', dados)
+
+def lista_notas(request):
+    return render(request,'lista_notas.html')
+
+def lista_salas(request):
+    return render(request,'lista_salas.html')
+
+
+def altera_cursos(request):
+    return render(request,'altera_cursos.html')
+
+def altera_matriculas(request):
+    return render(request,'altera_matriculas.html')
+
+def altera_notas(request):
+    return render(request,'altera_notas.html')
+
+def altera_salas(request):
+    return render(request,'altera_salas.html')
+
+
+
+
+
+
+
---
+++ Probluinfo/Pessoas/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render, redirect
+from django.db import transaction
+from django.contrib.auth.hashers import make_password
+from Pessoas.models import Pessoas, Perfis
+from Pessoas.forms import FormPessoas, FormPessoasAltera
+from ViewsProject.views import efetua_paginacao
+
+# Create your views here.
+@transaction.atomic
+def cadastra_pessoas(request):
+    perfil = Perfis.objects.filter(is_active=True).order_by('descricao')
+
+    if request.method == 'POST':
+        formPessoa = FormPessoas(request.POST or None)
+        if formPessoa.is_valid():
+            try:
+                with transaction.atomic():
+                    formPessoa.cleaned_data['password'] = make_password(formPessoa.cleaned_data['password'])
+                    Pessoas.objects.create(**formPessoa.cleaned_data)
+            except Exception as error:
+                print(error)
+            return redirect(cadastra_pessoas)
+    else:
+        formPessoa = FormPessoas()
+    print(formPessoa.errors)
+
+    dados = {
+                'perfis' : perfil,
+            }
+    return render(request,'cadastra_pessoas.html', dados)
+
+def lista_pessoas(request):
+    procura = request.GET.get('procura')
+    
+    if procura:
+        tipoPessoa = Pessoas.objects.filter(first_name__icontains=procura)
+    else:
+        tipoPessoa = Pessoas.objects.all()
+
+    total = tipoPessoa.count
+
+    dados = {
+                'tipos' : tipoPessoa,
+                'total' : total, 
+                'procura' : procura,
+                'porPagina' : efetua_paginacao(request, tipoPessoa)
+                
+            }
+
+    return render(request, 'lista_pessoas.html', dados)
+
+@transaction.atomic
+def altera_pessoas(request,id):
+    perfil = Perfis.objects.filter(is_active=True).order_by('descricao')
+    pessoas = Pessoas.objects.get(id=id)
+    data_nas = pessoas.dt_nascimento
+    data_nas = data_nas.strftime('%Y-%m-%d')
+    if request.method == 'POST':
+        form = FormPessoasAltera(request.POST, instance=pessoas)
+        if form.is_valid():
+            print(form.cleaned_data)
+            try:
+                with transaction.atomic():
+                    form.save()
+            except Exception as error:
+                print(error)
+            return redirect(lista_pessoas)
+    else:
+        form = FormPessoasAltera()
+    dados = {
+                'perfis' : perfil,
+                'pessoas' : pessoas,
+                'data_nas': data_nas,
+                'form':form
+            }
+    return render(request, 'altera_pessoas.html', dados)
+   
